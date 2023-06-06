fcff415be5a349b1ec87a39d6e33fcf413d204da
Chisom Chukwuma <chukwumachisom18@gmail.com>
2023-06-06 04:29:14 -1100

    made minor changes

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,14 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 import '../style/SideBar.css'
 import { AiOutlineHome } from 'react-icons/ai'
-import { BiWallet } from 'react-icons/bi'
-import { BiMessageRoundedDetail } from 'react-icons/bi'
-import { BsFillPeopleFill } from 'react-icons/bs'
+import {
+  BiWallet,
+  BiMessageRoundedDetail,
+  BiMicrophone,
+  BiMoviePlay,
+} from 'react-icons/bi'
+import { BsFillPeopleFill, BsGraphUp, BsCalendar4Event } from 'react-icons/bs'
 import { MdNotificationsNone } from 'react-icons/md'
-import { BsGraphUp } from 'react-icons/bs'
+import { RiArrowDropUpLine, RiArrowDropDownLine } from 'react-icons/ri'
+import { ImFilm } from 'react-icons/im'
+import { FiMusic } from 'react-icons/fi'
+import { TbDeviceGamepad2 } from 'react-icons/tb'
+import { MdMoreHoriz } from 'react-icons/md'
 import { Link, useLocation } from 'react-router-dom'
+import activity from '../assets/activity.png'
 
 const SideBar = () => {
+  const [show, setShow] = useState(false)
   const location = useLocation()
   return (
     <aside className='sidebar p-lg-5'>
@@ -120,7 +130,158 @@ const SideBar = () => {
           </Link>
         </div>
       </section>
-      <article></article>
+      <section>
+        <article className='side-firstArt mt-lg-3 d-flex'>
+          <img
+            src={activity}
+            alt='activity icon'
+            className='activity mt-lg-1'
+          />
+          <p className='ms-lg-4'>Activity</p>
+          <div className='ms-lg-2'>
+            {show === false ? (
+              <RiArrowDropUpLine
+                className='side-arrowIcon'
+                onClick={() => setShow(true)}
+              />
+            ) : (
+              <RiArrowDropDownLine
+                className='side-arrowIcon'
+                onClick={() => setShow(!true)}
+              />
+            )}
+          </div>
+        </article>
+        <section className='mt-lg-2'>
+          {show ? (
+            <section>
+              <div className='d-flex'>
+                <ImFilm
+                  className='side-icons'
+                  style={{
+                    color: location.pathname === '/Video' ? '#6821C3' : 'black',
+                  }}
+                />
+                <Link
+                  to='/Video'
+                  className='Link-sideText ms-lg-2'
+                  style={{
+                    color: location.pathname === '/Video' ? '#6821C3' : 'black',
+                  }}
+                >
+                  <p>Video</p>
+                </Link>
+              </div>
+              <div className='d-flex mt-lg-2'>
+                <FiMusic
+                  className='side-icons'
+                  style={{
+                    color: location.pathname === '/Music' ? '#6821C3' : 'black',
+                  }}
+                />
+                <Link
+                  to='/Music'
+                  className='Link-sideText ms-lg-2'
+                  style={{
+                    color: location.pathname === '/Music' ? '#6821C3' : 'black',
+                  }}
+                >
+                  <p>Music</p>
+                </Link>
+              </div>
+              <div className='d-flex mt-lg-2'>
+                <BiMicrophone
+                  className='side-icons'
+                  style={{
+                    color:
+                      location.pathname === '/Podcast' ? '#6821C3' : 'black',
+                  }}
+                />
+                <Link
+                  to='/Podcast'
+                  className='Link-sideText ms-lg-2'
+                  style={{
+                    color:
+                      location.pathname === '/Podcast' ? '#6821C3' : 'black',
+                  }}
+                >
+                  <p>Podcast</p>
+                </Link>
+              </div>
+              <div className='d-flex mt-lg-2'>
+                <BsCalendar4Event
+                  className='side-icons'
+                  style={{
+                    color: location.pathname === '/Event' ? '#6821C3' : 'black',
+                  }}
+                />
+                <Link
+                  to='/Event'
+                  className='Link-sideText ms-lg-2'
+                  style={{
+                    color: location.pathname === '/Event' ? '#6821C3' : 'black',
+                  }}
+                >
+                  <p>Event</p>
+                </Link>
+              </div>
+              <div className='d-flex mt-lg-2'>
+                <TbDeviceGamepad2
+                  className='side-icons'
+                  style={{
+                    color: location.pathname === '/Game' ? '#6821C3' : 'black',
+                  }}
+                />
+                <Link
+                  to='/Game'
+                  className='Link-sideText ms-lg-2'
+                  style={{
+                    color: location.pathname === '/Game' ? '#6821C3' : 'black',
+                  }}
+                >
+                  <p>Game</p>
+                </Link>
+              </div>
+              <div className='d-flex mt-lg-2'>
+                <BiMoviePlay
+                  className='side-icons'
+                  style={{
+                    color: location.pathname === '/Movie' ? '#6821C3' : 'black',
+                  }}
+                />
+                <Link
+                  to='/Movie'
+                  className='Link-sideText ms-lg-2'
+                  style={{
+                    color: location.pathname === '/Movie' ? '#6821C3' : 'black',
+                  }}
+                >
+                  <p>Movie</p>
+                </Link>
+              </div>
+              <div className='d-flex mt-lg-2'>
+                <MdMoreHoriz
+                  className='side-icons'
+                  style={{
+                    color: location.pathname === '/More' ? '#6821C3' : 'black',
+                  }}
+                />
+                <Link
+                  to='/More'
+                  className='Link-sideText ms-lg-2'
+                  style={{
+                    color: location.pathname === '/More' ? '#6821C3' : 'black',
+                  }}
+                >
+                  <p>More</p>
+                </Link>
+              </div>
+            </section>
+          ) : (
+            ''
+          )}
+        </section>
+      </section>
     </aside>
   )
 }
--- src/layouts/DashboardLayout.tsx
+++ src/layouts/DashboardLayout.tsx
@@ -1,7 +1,5 @@
 import React from 'react'
 import { Outlet } from 'react-router'
-import Sidebar from '../components/Sidebar/Sidebar'
-import Header from '../components/Header/Header'
 import NavBar from '../components/NavBar'
 import SideBar from '../components/SideBar'
 import '../style/NavBar.css'
@@ -10,12 +8,10 @@ const DashboardLayout = () => {
   return (
     <div className='dash-layout'>
       <NavBar />
-      {/* <Header /> */}
       <section className='d-flex mt-lg-2'>
         <SideBar />
         <Outlet />
       </section>
-      {/* <Sidebar /> */}
     </div>
   )
 }
