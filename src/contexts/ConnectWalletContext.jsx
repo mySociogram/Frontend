3c6cc165e0539162a0e50951f7772d61fc3b3ff3
Chisom Chukwuma <chukwumachisom18@gmail.com>
2023-06-27 22:32:53 -1100

    cleaned up

--- src/contexts/ConnectWalletContext.jsx
+++ src/contexts/ConnectWalletContext.jsx
@@ -1,5 +1,5 @@
-import React, { useState, createContext, useEffect, useContext } from 'react'
-import axios from 'axios'
+import React, { useState, createContext, useEffect } from 'react'
+// import axios from 'axios'
 
 import { useAccount, useConnect, useDisconnect } from 'wagmi'
 import { InjectedConnector } from 'wagmi/connectors/injected'
@@ -7,24 +7,15 @@ import { InjectedConnector } from 'wagmi/connectors/injected'
 export const ConnectWalletContext = createContext()
 
 const ConnectWalletContextProvider = ({ children }) => {
-  const [data, setData] = useState()
+  // const [data, setData] = useState()
   const [walletAddress, setWalletAddress] = useState('')
   const { address, isConnected, account } = useAccount()
+
   const { connect } = useConnect({
     connector: new InjectedConnector(),
   })
-  const { disconnect } = useDisconnect()
-useEffect(() => {
-    let walletAddress
-    const addressFromStorage = localStorage.getItem("address");
-    if (addressFromStorage) {
-      walletAddress = JSON.parse(addressFromStorage)
-      setWalletAddress(walletAddress)
-    }
-postAddress()
-  }, [walletAddress])
 
-      console.log(walletAddress, "address")
+  const { disconnect } = useDisconnect()
 
   const postAddress = () => {
     const user = {
@@ -48,25 +39,21 @@ postAddress()
         console.log(user)
       })
       .catch((e) => {
-        window.alert(e)
         console.log(e)
       })
   }
 
-  // const postAddress = async () => {
-  //   await axios
-  //     .post('http://localhost:3005/users', {
-  //       walletId: address,
-  //     })
-  //     .then((response) => {
-  //       setData(response.data)
-  //       window.alert(response.data)
-  //     })
-  //     .catch((error) => {
-  //       console.error('Error sending data:', error)
-  //     })
-  // }
-  
+  useEffect(() => {
+    let walletAddress
+    const addressFromStorage = localStorage.getItem('address')
+    if (addressFromStorage) {
+      walletAddress = JSON.parse(addressFromStorage)
+      setWalletAddress(walletAddress)
+    }
+    postAddress()
+  }, [walletAddress])
+
+  console.log(walletAddress, 'address')
 
   return (
     <ConnectWalletContext.Provider
